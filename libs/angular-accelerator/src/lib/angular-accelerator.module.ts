87f3a7006d2330ed29f706f4324223c35cad503e
KimFFVII <12371032+KimFFVII@users.noreply.github.com>
2025-08-27 10:05:26 +0200

    fix: imports reordering

--- libs/angular-accelerator/src/lib/angular-accelerator.module.ts
+++ libs/angular-accelerator/src/lib/angular-accelerator.module.ts
@@ -3,10 +3,13 @@ import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { TranslateModule } from '@ngx-translate/core'
-
 import { AppConfigService, UserService } from '@onecx/angular-integration-interface'
 import { AngularRemoteComponentsModule } from '@onecx/angular-remote-components'
-
+import {
+  MultiLanguageMissingTranslationHandler,
+  providePermissionChecker,
+  provideTranslationPathFromMeta,
+} from '@onecx/angular-utils'
 import { firstValueFrom, skip } from 'rxjs'
 import { AngularAcceleratorPrimeNgModule } from './angular-accelerator-primeng.module'
 import { ColumnGroupSelectionComponent } from './components/column-group-selection/column-group-selection.component'
@@ -14,28 +17,23 @@ import { CustomGroupColumnSelectorComponent } from './components/custom-group-co
 import { DataLayoutSelectionComponent } from './components/data-layout-selection/data-layout-selection.component'
 import { DataListGridSortingComponent } from './components/data-list-grid-sorting/data-list-grid-sorting.component'
 import { DataListGridComponent } from './components/data-list-grid/data-list-grid.component'
+import { DataLoadingErrorComponent } from './components/data-loading-error/data-loading-error.component'
 import { DataTableComponent } from './components/data-table/data-table.component'
 import { DataViewComponent } from './components/data-view/data-view.component'
 import { DiagramComponent } from './components/diagram/diagram.component'
+import { FilterViewComponent } from './components/filter-view/filter-view.component'
 import { GroupByCountDiagramComponent } from './components/group-by-count-diagram/group-by-count-diagram.component'
 import { InteractiveDataViewComponent } from './components/interactive-data-view/interactive-data-view.component'
 import { PageHeaderComponent } from './components/page-header/page-header.component'
-import { DataLoadingErrorComponent } from './components/data-loading-error/data-loading-error.component'
 import { SearchHeaderComponent } from './components/search-header/search-header.component'
 import { AdvancedDirective } from './directives/advanced.directive'
 import { IfBreakpointDirective } from './directives/if-breakpoint.directive'
 import { IfPermissionDirective } from './directives/if-permission.directive'
-import {
-  MultiLanguageMissingTranslationHandler,
-  providePermissionChecker,
-  provideTranslationPathFromMeta,
-} from '@onecx/angular-utils'
 import { SrcDirective } from './directives/src.directive'
 import { TooltipOnOverflowDirective } from './directives/tooltipOnOverflow.directive'
 import { DynamicPipe } from './pipes/dynamic.pipe'
 import { OcxTimeAgoPipe } from './pipes/ocxtimeago.pipe'
 import { DynamicLocaleId } from './utils/dynamic-locale-id'
-import { FilterViewComponent } from './components/filter-view/filter-view.component'
 
 export class AngularAcceleratorMissingTranslationHandler extends MultiLanguageMissingTranslationHandler {}
 
